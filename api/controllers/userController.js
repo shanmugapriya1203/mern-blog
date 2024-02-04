1c2299fbd6d38527d202fcc8de157681d8ac6a4e
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-02-04 21:44:33 +0530

    create test apirouter;

---
+++ api/controllers/userController.js
@@ -0,0 +1,4 @@
+export const test=(req,res)=>{
+    res.json({success:'Api is working',message:'Test is working'})
+}
+
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-
+import userRoutes from './routes/userRoute.js'
+import authRoutes from './routes/authRoute.js'
 dotenv.config()
+
 mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log('Connected to MongoDB')
@@ -16,3 +18,8 @@ const app=express()
 app.listen(3000,()=>{
     console.log('Server listening on port 3000')
 })
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+
+
