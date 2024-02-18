3e8472c11db18aac08992a734abdbd9b3f4271b3
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-02-18 21:33:45 +0530

    debug the error message

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+// index.js or wherever you initialize your app
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { store, persistor } from './redux/store.js'; // Import both store and persistor
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-<PersistGate persistor={persistor}>
-<Provider store={store}>
-  <App />
-  </Provider>
-</PersistGate>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
-  
-
-
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,22 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
 import { combineReducers } from '@reduxjs/toolkit';
-import { persistReducer} from 'redux-persist'
+import { persistReducer } from 'redux-persist';
 import { persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
+import storage from 'redux-persist/lib/storage';
+
 const rootReducer = combineReducers({
   user: userReducer,
 });
-const persistConfig={
-  key:'root',
-  storage,
-  version:1,
-}
 
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware:(getDefaultMiddleware)=>
-  getDefaultMiddleware({serializableCheck:false})
-})
-export const persistor= persistStore(store)
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store); // exporting persistor here
