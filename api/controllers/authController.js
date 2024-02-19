45cf319f1dde4179e98adf1a9f19681965cfdde3
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-02-19 22:01:10 +0530

    add Oauth functionaility

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -31,7 +31,7 @@ export const signin = async (req, res, next) => {
     try {
         const validUser = await User.findOne({ email });
         if (!validUser) {
-           return next(errorHandler(404, 'User not found'))
+            return res.status(404).json({ message: 'User not found' });
         }
         const validPassword = bcrypt.compareSync(password, validUser.password);
         if (!validPassword) {
@@ -41,11 +41,46 @@ export const signin = async (req, res, next) => {
             { id: validUser._id },
             process.env.JWT_SECRET_KEY
         );
-      const { password:pass,...rest}=validUser._doc;
+        const { password, ...rest } = validUser._doc;
         res.status(200).cookie('access_token', token, {
             httpOnly: true
         }).json(rest);
     } catch (error) {
         next(error);
     }
+}
+
+
+export const google = async (req, res, next) => {
+const { email ,name ,photoURL}=req.body;
+try {
+    const user= await User.findOne({email})
+    if(user){
+        const token= jwt.sign({id: user.id},process.env.JWT_SECRET_KEY)
+        const {password,...rest}= user._doc;
+        res.status(200).cookie('access_token',token,{
+            httpOnly: true,
+        }).json(rest)
+    }
+    else{
+        const generatedPassword= Math.random().toString(36).slice(-8)
+        const hashedPassword= bcryptjs.hashSync(generatedPassword,10)
+        const newUser= new User({
+            username:name.toLowerCase().split(' ').join(' ') + Math.random().toString(9).slice(-4),
+
+
+            email,
+            password:hashedPassword,
+            profilePicture:photoURL,
+        })
+        await newUser.save()
+        const token= jwt.sign({id: newUser.id},process.env.JWT_SECRET_KEY)
+        const {password,...rest}= newUser._doc;
+        res.status(200).cookie('access_token',token,{
+            httpOnly: true,
+        }).json(rest)
+    }
+} catch (error) {
+    
+}
 }
