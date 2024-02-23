c429fda6d1a89d2c60a0ebb2eef69157064b1e13
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-02-23 18:57:54 +0530

    use useLocation by using params

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
 const Dashboard = () => {
+  const location=useLocation()
+  const[tab,setTab]=useState('')
+  useEffect(()=>{
+    const urlParms=new URLSearchParams(location.search)
+    const tabFormUrl=urlParms.get('tab')
+    console.log(tabFormUrl)
+  })
   return (
-    <div>
+    <div className="">
+      {/* Sidebar */}
       
     </div>
+    
   )
 }
 
