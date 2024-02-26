1fab021407b59938249ff44483bef61450964847
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-02-26 21:21:37 +0530

    Basic setup function for profile picture change

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,14 +1,35 @@
 import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 const DashProfile = () => {
     const { currentUser}= useSelector((state)=>state.user)
+    const[imageFile,setImageFie]=useState(null)
+    const[imageFileUrl,setImageFileUrl]=useState(null)
+    const FilePickRef=useRef()
+    const handleImageChange=(e)=>{
+      const file= e.target.files[0]
+      if(file){
+        setImageFie(file)
+        setImageFileUrl(URL.createObjectURL(file))
+
+      }
+
+    }
+    useEffect(()=>{
+      if(imageFile){
+        uploadImage()
+      }
+    },[imageFile])
+    const handleImage=async()=>{
+      console.log('Uploading image')
+    }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-        <img src={currentUser.profilePicture} alt='user' className='rounded-full w-full h-full  object-coverborder-8 border-[lightgray]'/>
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={FilePickRef} hidden />
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>FilePickRef.current.click()}>
+        <img src={ imageFileUrl || currentUser.profilePicture} alt='user' className='rounded-full w-full h-full  object-coverborder-8 border-[lightgray]'/>
         </div>
 <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username}/>
 <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email}/>
