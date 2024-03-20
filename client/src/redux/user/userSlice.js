b502083d3c244f19bfad9690452a3535e788d16d
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-03-20 09:02:00 +0530

    state for deleteUser

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -35,10 +35,23 @@ const userSlice = createSlice({
         updateFailure:(state,action)=>{
             state.loading=false,
             state.error=action.payload
+        },
+        deleteUserStart:(state)=>{
+            state.loading=true,
+            state.error=null
+        },
+        deleteUserSuccess:(state,action)=>{
+            state.currentUser = null
+            state.loading=false,
+            state.error=null;
+        },
+        deleteUserFailure:(state,action)=>{
+            state.loading=false,
+            state.error=action.payload
         }
-    }
+    },
 });
 
-export const { signInStart, signInSuccess, signInFailure,updateStart,updateSuccess,updateFailure } = userSlice.actions; // Correct export of action creators
+export const { signInStart, signInSuccess, signInFailure,updateStart,updateSuccess,updateFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure } = userSlice.actions; // Correct export of action creators
 
 export default userSlice.reducer;
