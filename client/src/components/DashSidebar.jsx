78f8df31708de70ccb9aa30c8b2ba8e10da761f5
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-03-30 22:34:30 +0530

    add posts section to the sidebar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,10 +1,12 @@
 import { Sidebar, Label } from 'flowbite-react';
-import {HiArrowSmRight, HiUser} from 'react-icons/hi'
+import {HiArrowSmRight, HiDocumentText, HiUser} from 'react-icons/hi'
 import { useLocation ,Link} from "react-router-dom"
 import { useEffect ,useState} from "react"
+import { useSelector } from 'react-redux';
 const DashSidebar = () => {
     const location=useLocation()
     const[tab,setTab]=useState('')
+    const {currentUser}=useSelector(state=>state.user)
     useEffect(()=>{
       const urlParms=new URLSearchParams(location.search)
       const tabFormUrl=urlParms.get('tab')
@@ -16,13 +18,20 @@ const DashSidebar = () => {
     
     <Sidebar className='w-full md:w-56'>
        <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
             <Link to='/dashboard?tab=profile'>
 
            
-            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>
+            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={currentUser ? 'Admin' :'User' } labelColor='dark' as='div'>
                Profile
             </Sidebar.Item>
+            </Link>
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+              active={tab==='posts'}
+              icon={HiDocumentText}>
+                Posts
+              </Sidebar.Item>
             </Link>
             <Sidebar.Item  icon={HiArrowSmRight}  className='cursor-pointer'>
                Sign Out
