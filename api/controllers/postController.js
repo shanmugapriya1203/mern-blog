688d5c3ac9c95d2d0c61e0f8c06f7484f6aa8ee2
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-03-30 21:18:50 +0530

    add create post api route

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,3 +1,22 @@
-export const create=async(req,res,next)=>{
+import { errorHandler } from "../utils/error.js"
+import Post from './../models/postModel.js';
 
+export const create=async(req,res,next)=>{
+   
+if(!req.user.isAdmin){
+    return next(errorHandler(403,'You do not have permission to create a post'))
+}
+if(!req.body.title || !req.body.content){
+    return next(errorHandler(400,'Please fill all the fields'))
+}
+const slug= req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g,'')
+const newPost=new Post({
+    ...req.body,slug,userId:req.user.id
+});
+try {
+    const savedPost= await newPost.save()
+    res.status(201).json({message:'Post created successfully',post:savedPost})
+} catch (error) {
+    next(error)
+}
 }
--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { verifyToken } from './../utils/verifyUser';
-import { create } from '../controllers/postController';
+import { verifyToken } from './../utils/verifyUser.js';
+import { create } from '../controllers/postController.js';
 const router= express.Router();
 
 router.post('/create',verifyToken,create)
+export default router
