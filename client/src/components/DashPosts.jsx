e80471af874f5455db26448af9795412101913b0
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-04-02 18:58:06 +0530

    create api functionality for get all posts

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const DashPosts = () => {
+  const {currentUser} = useSelector((state) => state.user);
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setPosts(data.posts);
+         
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    
+      if(currentUser.isAdmin){
+        fetchPosts()
+      }
+    
+  }, [currentUser._id]);
+   // Run the effect whenever currentUser changes
+
   return (
     <div>
-      Posts
+    <h1>Hi</h1>
     </div>
-  )
-}
+  );
+};
 
-export default DashPosts
+export default DashPosts;
