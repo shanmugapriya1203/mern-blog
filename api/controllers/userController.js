5857a3ccef1cfed0452da0a3eabb3c0f769a6c50
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-04-05 08:06:53 +0530

    delete user according to admin status

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -49,7 +49,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.userId) {
+    if (!req.user.isAdmin && req.user.id !== req.params.userId) {
         return res.status(401).json({ message: 'You are not authorized to perform this action' });
     }
     try {
