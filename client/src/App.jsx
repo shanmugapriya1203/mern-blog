ea9e6507830094f5d613ec4ce9b4474f5299d499
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-04-16 07:39:33 +0530

    compelete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/PrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
 function App() {
   return (
 <BrowserRouter>
@@ -31,6 +32,7 @@ function App() {
 
 
   <Route path='/projects' element={<Projects/>}/>
+  <Route path='/post/:postSlug' element={<PostPage/>}/>
 </Routes>
 <FooterCom/>
 </BrowserRouter>
