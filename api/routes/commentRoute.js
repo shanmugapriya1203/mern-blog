e589c9c9dad681c86e7aa324457a94cd5e40f387
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-04-17 21:10:11 +0530

    small error

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const router= express.Router()
-import { verifyToken } from './../utils/verifyUser';
-import { createComment } from '../controllers/commentController';
+import { verifyToken } from './../utils/verifyUser.js';
+import { createComment } from '../controllers/commentController.js';
 
 router.post('/create', verifyToken, createComment);
+export default router
