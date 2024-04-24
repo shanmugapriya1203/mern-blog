80a0c5c32127d9eab554581b056c42ac140d8fb7
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-04-24 07:38:19 +0530

    like coment functionality

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router= express.Router()
 import { verifyToken } from './../utils/verifyUser.js';
-import { createComment, getPostComments } from '../controllers/commentController.js';
+import { createComment, getPostComments, like } from '../controllers/commentController.js';
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likecomment/:commentId', verifyToken,like);
 export default router
